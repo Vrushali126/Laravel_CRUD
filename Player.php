0249dc46de3f017cf0b4f8bdcba5cf6baf41c8a9
Vrushali126 <vrushali.gaikwad@grawlixsoft.com>
2025-04-01 18:46:24 +0530

    Add files via upload

---
+++ Player.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Player extends Model
+{
+    protected $table = 'team';  // <-- Set your table name here
+
+    //
+}
